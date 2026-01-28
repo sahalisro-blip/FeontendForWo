2c4a65cba3d7ccce24be0d49da8468c1bdddfd06
Sahal Belam <belamsahal8@gmail.com>
2026-01-28 22:10:13 +0530

    adding FE for Work Order

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+function App() {
+
+  return (
+    <>
+      <h1 className='text-amber-500 text-3xl'>hello there</h1>
+      <Button />
+    </>
+  )
+}
+
+export default App
